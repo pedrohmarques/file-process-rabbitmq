b465046c79fcf140fe2ae35fc05b32352403823e
Pedro Almeida <pdhmarques@gmail.com>
2026-07-27 18:27:48 -0300

    Finishing project

--- frontend/src/app/view/file-process/components/processing-files/processing-files.ts
+++ frontend/src/app/view/file-process/components/processing-files/processing-files.ts
@@ -12,4 +12,15 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class ProcessingFiles {
   files = input<any[]>([])
+
+  formatFileSize(bytes: number, decimals = 2): string {
+    if (bytes === 0) return '0 Bytes';
+  
+    const k = 1024;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+  
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(decimals))} ${sizes[i]}`;
+  }
 }
